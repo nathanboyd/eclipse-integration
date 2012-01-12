6bcd24234fb97b7d4c6adc83a162a9ca79256daf
nathan.boyd <nathan.boyd@duffman.datagister.com>
2012-01-12 09:48:28 -0500

    Changed the text of the message

---
+++ eclipse integration/helloworld/src/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author nathan.boyd
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("change made");
+		
+	}
+
+}
